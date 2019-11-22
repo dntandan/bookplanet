0611dd71b3a5e7c6c57bd8e5d6f3a82e49605f4b
dntandan <deepnarayan006@gmail.com>
2019-11-22 22:59:17 +0530

    tiltle updated

--- single.php
+++ single.php
@@ -25,7 +25,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Home | Book Planet</title>
+    <title>Description | Book Planet</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="all,follow">
@@ -171,7 +171,7 @@
 	</div>
 	<!--container.//-->
 
-<!-- Start Book Details-->
+<!-- End Book Details-->
 
 
       </div>
